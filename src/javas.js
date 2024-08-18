53533ff24626113c58ba9aa1203f27131dd80abc
sarahatesyou <sarafcso@gmail.com>
2024-08-18 18:57:04 +0100

    i'm just trying stuff at this point

--- src/javas.js
+++ src/javas.js
@@ -1,3 +1,33 @@
+function formatDate(date) {
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let minutes = date.getMinutes().toString().padStart(2, "0");
+  return `${day} ${hours}:${minutes}`;
+}
+
+function submitSearch(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#city-search");
+  searchCity(searchInput.value);
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", submitSearch);
+
+function searchCity(city) {
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=o091fdfe309a88f508fe60bcaa4tc41a`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function refreshWeather(response) {
   let temperatureElement = document.querySelector("#current-temperature-value");
   let temperature = response.data.temperature.current;
@@ -16,34 +46,4 @@ function refreshWeather(response) {
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
-}
-
-function formatDate(date) {
-  let hours = date.getHours();
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  let day = days[date.getDay()];
-  let minutes = date.getMinutes().toString().padStart(2, "0");
-}
-
-function submitSearch(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#city-search");
-
-  searchCity(searchInput.value);
-}
-
-let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", submitSearch);
-
-function searchCity(city) {
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=o091fdfe309a88f508fe60bcaa4tc41a`;
-  axios.get(apiUrl).then(refreshWeather);
 }
