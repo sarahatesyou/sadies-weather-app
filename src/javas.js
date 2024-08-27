2ff889d03eebe4e9c8fea0fd509e0134da887bd8
sarahatesyou <sarafcso@gmail.com>
2024-08-27 20:04:43 +0100

    added weekly forecast

--- src/javas.js
+++ src/javas.js
@@ -16,6 +16,7 @@ function refreshData(response) {
   windSpeed.innerHTML = `${response.data.wind.speed}km/h`;
   currentTime.innerHTML = formatDate(date);
   emoji.innerHTML = `<img src="${response.data.condition.icon_url}" class="current-temperature-emoji" id="emoji">`;
+  weeklyForecast(response.data.city);
 }
 
 function formatDate(date) {
@@ -46,6 +47,13 @@ function submitSearch(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function weeklyForecast(city) {
   let apiKey = "o091fdfe309a88f508fe60bcaa4tc41a";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
@@ -54,6 +62,29 @@ function weeklyForecast(city) {
 
 function displayForecast(response) {
   let forecastHtml = "";
+
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `<div class="weekly-forecast-day">
+        <div class="weekly-forecast-date">${formatDay(day.time)}</div>
+
+        <img src="${day.condition.icon_url}" class="weekly-forecast-icon" />
+        <div class="weekly-forecast-temperatures">
+          <div class="weekly-forecast-temperature">
+            <strong>${Math.round(day.temperature.maximum)}º</strong>
+          </div>
+          <div class="weekly-forecast-temperature">${Math.round(
+            day.temperature.minimum
+          )}º</div>
+        </div>
+      </div>`;
+    }
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
 }
 
 let searchFormElement = document.querySelector("#search-form");
